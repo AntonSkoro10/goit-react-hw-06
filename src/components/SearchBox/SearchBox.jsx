83cfb706213bc0332e9ad219fdd1074ce661d3dc
AntonSkoro10 <antonskoro112@gmail.com>
2024-08-18 18:21:37 +0300

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { changeFilter } from '../../redux/filtersSlice';
 
 export default function SearchBox() {
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.filters.filter);
 
   const handleChange = event => {
     dispatch(changeFilter(event.target.value));
   };
 
-  return <input type="text" placeholder="Search" onChange={handleChange} />;
+  return (
+    <input
+      type="text"
+      placeholder="Пошук"
+      value={filter} // Встановлюємо значення інпуту на поточний фільтр
+      onChange={handleChange}
+    />
+  );
 }
